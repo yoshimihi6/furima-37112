6c7cd9344b73fd0340c9aafe0521ee57b33f51d8
yoshimihi6 <yoooshimi.36@gmail.com>
2021-11-09 18:59:23 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
 end
