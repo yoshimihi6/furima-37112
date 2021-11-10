0630b58a3ab58fe854642d8e2fcb5a40f0bcdc40
yoshimihi6 <yoooshimi.36@gmail.com>
2021-11-10 20:41:17 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :product_condition_id, :shipping_charge_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :product_condition_id, :shipping_charge_id, :prefecture_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
 end
 
