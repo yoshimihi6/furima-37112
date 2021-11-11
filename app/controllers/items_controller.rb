cc649deb52e4ea826c239ab61a3ddb32b610d8b9
yoshimihi6 <yoooshimi.36@gmail.com>
2021-11-11 18:54:09 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -13,5 +17,6 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :product_condition_id, :shipping_charge_id, :prefecture_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
 
