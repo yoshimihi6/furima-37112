50098541ea20942d6bf9952b113b782bd1e007f4
yoshimihi6 <yoooshimi.36@gmail.com>
2021-11-12 20:49:52 +0900

    テストコード修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,12 +2,9 @@ window.addEventListener('load', () => {
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
   const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(Math.floor(inputValue * 0.1));
   const Profit = document.getElementById("profit");
     Profit.innerHTML = (Math.floor(inputValue * 0.9));
-    console.log(Math.floor(inputValue * 0.9));
 })
 });
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,6 +63,46 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.product_condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank")
+      end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.schedule_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Schedule can't be blank")
+      end
+      it '価格が300円未満では出品できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
